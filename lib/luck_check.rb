e6c15fe973b28aa3040ca8e897491f155d9703fd
Katharine Chen & Scott Dudley <student@epicodus.com>
2014-07-23 16:21:07 -0700

    More refactoring

--- lib/luck_check.rb
+++ lib/luck_check.rb
@@ -1,7 +1,6 @@
 def luck_check(string)
   stringArray = string.split("").map { |x| x.to_i }
-  return false if stringArray.length == 0
   first_half = stringArray[0..stringArray.length/2 -1].inject {|sum, x| sum + x}
   second_half = stringArray[(stringArray.length/2.0).ceil..-1].inject {|sum, x| sum + x}
-  first_half == second_half
+  first_half == second_half && second_half != nil
 end
