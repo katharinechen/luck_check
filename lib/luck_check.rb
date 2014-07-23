f183c2c2bc1718c5fe4812f5599d364d862ea0f6
Katharine Chen & Scott Dudley <student@epicodus.com>
2014-07-23 15:59:44 -0700

    Refactored more

--- lib/luck_check.rb
+++ lib/luck_check.rb
@@ -1,21 +1,7 @@
-
 def luck_check(string)
-
-  stringArray = string.split("")
-
-  if stringArray.length == 0
-    return false
-  end
-
-  stringArray.map! { |x| x.to_i }
-
-  if stringArray.length.even?
-    first_half = stringArray[0..stringArray.length/2 -1].inject {|sum, x| sum + x}
-    second_half = stringArray[stringArray.length/2..-1].inject {|sum, x| sum + x}
-    return first_half == second_half
-  else
-    first_half = stringArray[0..stringArray.length/2 -1].inject {|sum, x| sum + x}
-    second_half = stringArray[(stringArray.length/2 + 1)..-1].inject {|sum, x| sum + x}
-    return first_half == second_half
-  end
+  stringArray = string.split("").map { |x| x.to_i }
+  return false if stringArray.length == 0
+  first_half = stringArray[0..stringArray.length/2 -1].inject {|sum, x| sum + x}
+  second_half = stringArray[(stringArray.length/2.0).ceil..-1].inject {|sum, x| sum + x}
+  first_half == second_half
 end
