ae27517d9d68a66b9f9672a2fe8cc3eb48109728
Katharine Chen <katharinechen.ny@gmail.com>
2014-09-23 15:46:14 -0700

    Fix README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+#Luck Check
+
+##About
+This project was completed by Katharine Chen and Scott Dudley for Epicodus (Week#3 Day #3). 
+
+##Description 
+The objective of this project is to create a method that checks if a ticket is lucky. In some countries of former Soviet Union there was a belief about lucky tickets. A transport ticket of any sort was believed to be lucky if the sum of the digits on the left half of its number was equal to the sum of digits on the right half.
+
+##Functionality 
+```ruby
+luck_check("43252") ##return true 
+luck_check("4359")  ##return false 
+```
+
+##License
+MIT
--- spec/luck_check_spec.rb
+++ spec/luck_check_spec.rb
@@ -5,23 +5,18 @@ describe 'luck_check' do
   it ("checks if a string with an even number and tells you if it is lucky") do
     expect(luck_check("003111")).to(eq(true))
   end
-
   it ("checks if a string with an odd number of digits is lucky") do
     expect(luck_check("43252")).to(eq(true))
   end
-
   it ("checks if an even digit number is not lucky") do
     expect(luck_check("4359")).to(eq(false))
-  end
-
+  end 
   it ("checks if an odd digit number is not lucky") do
     expect(luck_check("12345")).to(eq(false))
   end
-
   it ("takes an empty string and says it's not lucky") do
     expect(luck_check("")).to(eq(false))
   end
-
   it ("takes a string with non-integer character and says it is lucky") do
     expect(luck_check("2e11")).to(eq(true))
   end
